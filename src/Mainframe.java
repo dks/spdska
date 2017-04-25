61539a66867d49b0dc394d4a62ffa9b03ef9614d
Dvortsov Konstantin <kostya.dvortsov>
2017-04-25 20:59:05 +1100

    Setting up some basic startup windows

--- src/Mainframe.java
+++ src/Mainframe.java
@@ -28,18 +28,33 @@ public class Mainframe{
 			JDesktopPane desktop = new JDesktopPane();
 			jf.setContentPane(desktop);
 			desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+			desktop.setBackground(Color.GRAY);
 			JInternalFrame jint = new JInternalFrame();
 			//JInternalFrame jint = new JInternalFrame("Status",true,true,true,true);
 			desktop.add(jint);
-			jint.add(new JLabel("TEST"));
 			Dimension mfsize = jf.getSize();
 			Insets mwi = jf.getInsets();
 			int mww = mfsize.width - mwi.left - mwi.right;
 			int mwh = mfsize.height - mwi.top - mwi.bottom;
-			jint.setSize(mww/3,mwh);
+			//jint.setSize(mww/3,mwh);
+			jint.setBounds(mww/3*2,bounds.y,mww/3,mwh);
 			((BasicInternalFrameUI) jint.getUI()).setNorthPane(null);
 			jint.setBorder(null);
+			jint.setBackground(Color.BLACK);
+			JLabel statusWindow = new JLabel();
+			statusWindow.setForeground(Color.YELLOW);
+			jint.setLayout(new BorderLayout());
+			jint.add(statusWindow,BorderLayout.NORTH);
 			jint.setVisible(true);
+
+			statusWindow.setText("<html></html>");
+			StringBuffer sb = new StringBuffer(statusWindow.getText());
+			sb.insert(sb.length()-7,"Подготовка к работе программы..."+"<br>");
+			statusWindow.setText(sb.toString());
+			sb.insert(sb.length()-7,"Подключение к базе данных..."+"<br>");
+			statusWindow.setText(sb.toString());
+			sb.insert(sb.length()-7,"Подключение установлено!"+"<br>");
+			statusWindow.setText(sb.toString());
 		}});
 	}
 }
