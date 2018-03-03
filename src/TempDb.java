f51cfbd8fdfc16d40cd98ab9850be78d6e8183f4
Konstantin Dvortsov <kostya.dvortsov@gmail.com>
2018-03-03 19:50:17 +1100

    Начал наброски архитектуры данных

---
+++ src/TempDb.java
@@ -0,0 +1,9 @@
+import dk_util.*;
+
+public class TempDb{
+  public static void main(String[] args){
+    H2DB.sim();
+
+
+  }
+}
--- src/dk_util/H2DB.java
+++ src/dk_util/H2DB.java
@@ -5,6 +5,7 @@ import java.sql.*;
 
 public class H2DB{
 	private static H2DB instance;
+	protected static Connection conn;
 	public static void init(){
 		if (instance == null) { instance = new H2DB(); } 
 		try{
@@ -24,4 +25,77 @@ public class H2DB{
 			Log.m("ERROR(3): "+e);
 		}
 	}
+  public static void sim(){
+    int lastparent=0;
+    int lastchild=0;
+
+    System.out.println("TEST H2DB");
+		if (instance == null) { instance = new H2DB(); } 
+		try{
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:res/test","sa","");
+			Statement s = conn.createStatement();
+      ResultSet rs;
+
+			s.execute("DROP TABLE data2;");
+			s.execute("DROP TABLE dataref;");
+			s.execute("CREATE TABLE IF NOT EXISTS data2(id int AUTO_INCREMENT primary key,txt VARCHAR(50));");
+			s.execute("CREATE TABLE IF NOT EXISTS dataref(chid int ,parid int);");
+			s.executeUpdate("INSERT INTO data2 VALUES (null,'Здание');");
+			rs = s.executeQuery("SELECT LAST_INSERT_ID();");
+			while(rs.next()){ lastparent = rs.getInt(1); }
+      
+      insertObj(lastparent,"Стена1");
+      insertObj(lastparent,"Стена2");
+      insertObj(lastparent,"Стена3");
+      insertObj(lastparent,"Стена4");
+      insertObj(lastparent,"Перекрытие1");
+      insertObj(lastparent,"Перекрытие2");
+      insertObj(lastparent,"Объем1");
+      linkObj(8,2);
+      linkObj(8,3);
+      linkObj(8,4);
+      linkObj(8,5);
+
+			rs = s.executeQuery("SELECT * FROM data2;");
+			while(rs.next()){
+				System.out.println(rs.getInt(1)+" "+rs.getString(2));
+			}
+      System.out.println("----------------");
+			rs = s.executeQuery("SELECT * FROM dataref;");
+			while(rs.next()){
+				System.out.println(rs.getInt(1)+" "+rs.getInt(2));
+			}
+			rs.close();
+			conn.close();
+		}catch(Exception e){
+      System.out.println("ERROR(3): "+e);
+			//Log.m("ERROR(3): "+e);
+		}
+  }
+  public static int insertObj(int refparent, String objType){
+    int lastrec=0;
+    try{
+      int lastchild=0;
+      Statement s = conn.createStatement();
+      s.executeUpdate("INSERT INTO data2 VALUES (null,'"+objType+"');");
+      ResultSet rs = s.executeQuery("SELECT LAST_INSERT_ID();");
+      while(rs.next()){ lastchild = rs.getInt(1); }
+      lastrec=lastchild;
+      s.executeUpdate("INSERT INTO dataref VALUES ("+lastchild+","+refparent+");");
+    }catch(Exception e){
+      System.out.println("ERROR(): "+e);
+			//Log.m("ERROR(3): "+e);
+		}
+    return lastrec;
+  }
+  public static void linkObj(int refparent, int refchild){
+    try{
+      Statement s = conn.createStatement();
+      s.executeUpdate("INSERT INTO dataref VALUES ("+refchild+","+refparent+");");
+    }catch(Exception e){
+      System.out.println("ERROR(): "+e);
+			//Log.m("ERROR(3): "+e);
+		}
+  }
 }
